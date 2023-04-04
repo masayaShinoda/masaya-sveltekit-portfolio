4d5d1a4e327249a8944aa62ecbcb0c2e7c42f3a0
Masaya Shida <masayashida@gmail.com>
2023-04-04 18:42:34 +0700

    working desktop gallery

---
+++ src/routes/graphic-design/data.js
@@ -0,0 +1,45 @@
+const gfx_projects = [
+    {
+        industry: "Finance & Technology",
+        items: []
+    },
+    {
+        industry: "Streetwear",
+        items: []
+    },
+    {
+        industry: "Automotives",
+        items: []
+    },
+    {
+        industry: "Food & Drinks",
+        items: []
+    },
+    {
+        industry: "E-sports",
+        items: [
+            {
+                src: "rog-masters-csgo-01_result.jpg",
+                alt: "graphic design portfolio, industry: esports, item: 1",
+            },
+            {
+                src: "ys-water-festival-01_result.jpg",
+                alt: "graphic design portfolio, industry: esports, item: 2",
+            },
+            {
+                src: "front-room-window-canvas-01_result.jpg",
+                alt: "graphic design portfolio, industry: esports, item: 3",
+            },
+            {
+                src: "mpadmockup_result.jpg",
+                alt: "graphic design portfolio, industry: esports, item: 4",
+            },
+            {
+                src: "ys-business-card-mockup_result.jpg",
+                alt: "graphic design portfolio, industry: esports, item: 5",
+            },
+        ]
+    },
+]
+
+export default { gfx_projects }
