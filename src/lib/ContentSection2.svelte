ef2425fcc80cd0d947046efca1c1a4c3251550ec
Masaya Shida <masayashida@gmail.com>
2023-07-27 23:00:20 +0700

    update logos

--- src/lib/ContentSection2.svelte
+++ src/lib/ContentSection2.svelte
@@ -1,80 +1,104 @@
 <script>
-import { getContext, onMount } from "svelte"
+	// import { getContext, onMount } from "svelte"
 
-const technologies_icons = [
-    {
-        categ: "Web Technologies",
-        icons: [
-            "html-css.svg",
-            "javascript.svg",
-            "nextjs.svg",
-            "react.svg",
-            "svelte.svg",
-        ]
-    },
-    {
-        categ: "UI Design",
-        icons: [
-            "figma.svg",
-            "adobe-xd.svg",
-        ],
-    },
-    {
-        categ: "Graphic Design",
-        icons: [
-            "adobe-illustrator.svg",
-            "adobe-photoshop.svg", 
-        ]            
-    }
-]
+	const technologies_icons = [
+		{
+			categ: 'Web Technologies',
+			icons: [
+				{
+					fileName: 'html-css.svg',
+					width: '62',
+					height: '40'
+				},
+				{
+					fileName: 'javascript.svg',
+					width: '40',
+					height: '40'
+				},
+				{
+					fileName: 'nextjs.svg',
+					width: '66',
+					height: '40'
+				},
+				{
+					fileName: 'react.svg',
+					width: '40',
+					height: '40'
+				},
+				{
+					fileName: 'svelte.svg',
+					width: '128',
+					height: '32'
+				},
+				{
+					fileName: 'django.svg',
+					width: '92',
+					height: '32'
+				}
+			]
+		},
+		{
+			categ: 'UI Design',
+			icons: [
+				{
+					fileName: 'figma.svg',
+					width: '40',
+					height: '40'
+				},
+				{
+					fileName: 'adobe-xd.svg',
+					width: '40',
+					height: '40'
+				}
+			]
+		},
+		{
+			categ: 'Graphic Design',
+			icons: [
+				{
+					fileName: 'adobe-illustrator.svg',
+					width: '40',
+					height: '40'
+				},
+				{
+					fileName: 'adobe-photoshop.svg',
+					width: '40',
+					height: '40'
+				}
+			]
+		}
+	];
 
-// onMount(() => {
-//     // react to change in <body> data attribute
-//     const body = document.body
-
-//     const observer = new MutationObserver(mutations => {
-//       mutations.forEach(record => {
-//         if(record.type === 'attributes') {
-//           const changedAttrName  = record.attributeName;    
-//           const newValue = record.target.getAttribute(changedAttrName);
-//           console.log(`Attribute changed! New value for '${changedAttrName}' : ${newValue}`);
-//         }
-//       });
-//     });
-    
-//     observer.observe(body, {
-//       attributes: true
-//     });
-// })
-
-console.log(getContext("themeContext"))
+	// console.log(getContext("themeContext"))
 </script>
 
-
 <section class="content_section_2_container">
-    <div class="content">
-        <h3>Skillset</h3>
-        <p>I'm constantly learning different technologies and software to complete projects efficiently.</p>
-        <div class="technology_icons_container">
-        {#each technologies_icons as { categ, icons }}
-            <div class="box">
-                <h4>{categ}</h4>
-                <div>
-                    {#each icons as icon}
-                        <img 
-                            src={`/images/${icon}`} 
-                            alt={icon.slice(0, icon.length - 4).replace("-", " ")} 
-                            title={icon.slice(0, icon.length - 4).replace("-", " ")}  
-                        />
-                    {/each}
-                </div>
-            </div>
-        {/each}
-        </div>
-
-    </div>
+	<div class="content">
+		<h3>Skillset</h3>
+		<p>
+			I'm constantly learning different technologies and software to complete projects efficiently.
+		</p>
+		<div class="technology_icons_container">
+			{#each technologies_icons as { categ, icons }}
+				<div class="box">
+					<h4>{categ}</h4>
+					<div>
+						{#each icons as icon}
+							<img
+								src={`/images/${icon.fileName}`}
+								alt={icon.fileName.slice(0, icon.fileName.length - 4).replace('-', ' ')}
+								title={icon.fileName.slice(0, icon.fileName.length - 4).replace('-', ' ')}
+								width={icon.width}
+								height={icon.height}
+							/>
+						{/each}
+					</div>
+				</div>
+			{/each}
+		</div>
+	</div>
 </section>
 
 <style>
-@import "./ContentSection2.module.css";
+	@import './ContentSection2.module.css';
 </style>
