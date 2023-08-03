775c7d07e65fd320d67376da8a374fce11a72a38
Masaya Shida <masayashida@gmail.com>
2023-08-03 23:22:59 +0700

    update hero img size

--- src/lib/home/Hero.svelte
+++ src/lib/home/Hero.svelte
@@ -1,29 +1,17 @@
 <section class="hero_container">
-    <div class="hero_content">
-        <span>
-            <h1>
-                <span class="subtitle">
-                    Hi, I'm Masaya!
-                </span>
-                <span class="title">
-                    I help ideas achieve a quality online presence.
-                </span>
-            </h1>
-            <a href="#portfolio" class="action_btn">
-                See my Projects →
-            </a>
-        </span>
-        <!-- <img src="/images/shinjang.png" alt="Hero" width="200" height="400" style="object-fit: contain" /> -->
-        <img 
-        src="/images/g14-shadow.png"
-        alt="ASUS G14 laptop, decorative."
-        width="320"
-        height="240"
-        />
-    </div>
+	<div class="hero_content">
+		<span>
+			<h1>
+				<span class="subtitle"> Hi, I'm Masaya! </span>
+				<span class="title"> I help ideas achieve a quality online presence. </span>
+			</h1>
+			<a href="#portfolio" class="action_btn"> See my Projects → </a>
+		</span>
+		<!-- <img src="/images/shinjang.png" alt="Hero" width="200" height="400" style="object-fit: contain" /> -->
+		<img src="/images/g14-shadow.png" alt="ASUS G14 laptop, decorative." width="300" height="200" />
+	</div>
 </section>
 
-
 <style>
-@import "./Hero.module.css";
+	@import './Hero.module.css';
 </style>
