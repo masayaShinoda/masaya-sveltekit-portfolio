75d2cb85216568d90e15e4668c5932dfb9aa399d
Masaya Shida <masayashida@gmail.com>
2023-08-03 18:58:52 +0700

    change to rewrite

--- src/lib/courses/CoursesNav.svelte
+++ src/lib/courses/CoursesNav.svelte
@@ -1,5 +1,22 @@
 <script>
+	import { writable } from 'svelte/store';
 	import ThemeToggler from '$lib/components/ThemeToggler.svelte';
+
+	const mobileNavActive = writable(false);
+
+	let isActive;
+
+	mobileNavActive.subscribe((value) => {
+		isActive = value;
+	});
+	function toggleMobileNav() {
+		mobileNavActive.update((prevValue) => !prevValue);
+		if (isActive) {
+			document.body.style.overflowY = 'hidden';
+		} else {
+			document.body.style.overflowY = 'scroll';
+		}
+	}
 </script>
 
 <section lang="km" class="nav_container">
@@ -9,11 +26,27 @@
 		</a>
 		<div class="nav_links">
 			<nav>
-				<!-- <a href="#">វគ្គសិក្សា</a>
-				<a href="#">អ្នកបង្រៀន</a> -->
+				<a href="#">វគ្គសិក្សា</a>
+				<a href="#">អ្នកបង្រៀន</a>
 			</nav>
 			<ThemeToggler />
 		</div>
+		<button
+			class={`mobile_nav_btn ${isActive ? 'close' : null}`}
+			on:click={toggleMobileNav}
+			aria-label={`${isActive ? 'Close' : 'Open'} Menu`}
+		>
+			<span aria-hidden="true" />
+			<span aria-hidden="true" />
+			<span aria-hidden="true" />
+		</button>
+	</div>
+	<div class={`mobile_nav_container ${isActive ? 'active' : null}`}>
+		<nav class="mobile_nav_drawer">
+			<a href="#" on:click={toggleMobileNav}>វគ្គសិក្សា</a>
+			<a href="#" on:click={toggleMobileNav}>គ្រូបង្រៀន</a>
+		</nav>
+		<div class="remaining" on:click={toggleMobileNav} />
 	</div>
 </section>
 
