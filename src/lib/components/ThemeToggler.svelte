8e0612812ac55dc5746771107a53163cc44ac6e3
Masaya Shida <masayashida@gmail.com>
2023-09-03 01:57:58 +0700

    update theme toggling icon, functionality

--- src/lib/components/ThemeToggler.svelte
+++ src/lib/components/ThemeToggler.svelte
@@ -1,163 +1,46 @@
 <script>
-	import { browser } from '$app/environment';
-	import { onMount } from 'svelte';
+	import MoonLine from '$lib/components/icons/moon-line.svelte';
+	import SunLine from '$lib/components/icons/sun-line.svelte';
+	import { themeStore } from '../../stores';
 
-	onMount(() => {
-		if (localStorage.getItem('theme') === 'dark') {
-			document.getElementById('toggle_checkbox').checked = true;
-		} else {
-			document.getElementById('toggle_checkbox').checked = false;
-		}
+	let theme;
+
+	themeStore.subscribe((value) => {
+		theme = value;
 	});
-	function toggleTheme(e) {
-		if (browser) {
-			if (e.target.checked) {
-				localStorage.setItem('theme', 'dark');
-				document.querySelector('body').dataset.theme = 'dark';
-			} else {
-				localStorage.setItem('theme', 'light');
-				document.querySelector('body').dataset.theme = 'light';
-			}
-		}
+
+	function toggleTheme() {
+		themeStore.update((theme) => (theme === 'light' ? 'dark' : 'light'));
 	}
 </script>
 
 <div class="theme_toggle">
-	<input type="checkbox" id="toggle_checkbox" on:change={toggleTheme} />
-	<label for="toggle_checkbox" title="Toggle website theme.">
-		<div id="star">
-			<div class="star" id="star-1">★</div>
-			<div class="star" id="star-2">★</div>
-		</div>
-		<div id="moon" />
-	</label>
+	<button on:click={toggleTheme} title="Toggle website theme." aria-label="Toggle website theme.">
+		{#if theme === 'dark'}
+			<SunLine />
+		{:else}
+			<MoonLine />
+		{/if}
+	</button>
 </div>
 
 <style>
-	.theme_toggle {
-		--multiplier: 0.425;
-		/* outline: 1px solid red; */
-	}
-	@media screen and (max-width: 64em) {
-		.theme_toggle {
-			--multiplier: 0.375;
-		}
-	}
-	@media screen and (max-width: 48em) {
-		.theme_toggle {
-			--multiplier: 0.425;
-		}
-	}
-
-	#toggle_checkbox {
-		display: none;
-	}
-
-	.theme_toggle label {
-		display: block;
-		width: calc(116px * var(--multiplier));
-		height: calc(56px * var(--multiplier));
-		/* margin: 0 auto; */
-		background-color: var(--clr_secondary);
-		border-radius: calc(56px * var(--multiplier));
-		transform: translateY(-50%);
+	.theme_toggle button {
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		padding: 0.5rem;
 		cursor: pointer;
-		transition: 0.3s ease background-color;
-		overflow: hidden;
 
-		transform: scale(1);
-		margin: auto 0 auto 0.875rem;
-
-		/* transform: scale(var(--multiplier)); */
-		-webkit-user-select: none; /* Safari */
-		-ms-user-select: none; /* IE 10 and IE 11 */
-		user-select: none; /* Standard syntax */
+		background: none;
+		border: 1px solid var(--clr_grey_shade_a);
+		border-radius: 100vmax;
+		transition: transform 50ms ease-out;
 	}
-	@media screen and (max-width: 64em) {
-		.theme_toggle label {
-			margin: auto 0 auto 0.75rem;
-		}
+	[data-theme='dark'] .theme_toggle button {
+		filter: invert(1);
 	}
-	@media screen and (max-width: 48em) {
-		.theme_toggle label {
-			margin: auto 0;
-		}
-	}
-
-	#star {
-		position: absolute;
-		top: calc(13px * var(--multiplier));
-		left: calc(13px * var(--multiplier));
-		width: calc(30px * var(--multiplier));
-		height: calc(30px * var(--multiplier));
-		background-color: #fafd0f;
-		transform: scale(1);
-		border-radius: 50%;
-		transition: 0.3s ease top, 0.3s ease left, 0.3s ease transform, 0.3s ease background-color;
-		z-index: 1;
-	}
-
-	#star-1 {
-		position: relative;
-	}
-
-	#star-2 {
-		position: absolute;
-		transform: rotateZ(36deg);
-	}
-
-	.theme_toggle .star {
-		top: 0;
-		left: calc(-7px * var(--multiplier));
-		font-size: calc(54px * var(--multiplier));
-		line-height: calc(28px * var(--multiplier));
-		color: #fafd0f;
-		transition: 0.3s ease color;
-	}
-
-	#moon {
-		position: absolute;
-		bottom: calc(-52px * var(--multiplier));
-		right: calc(8px * var(--multiplier));
-		width: calc(40px * var(--multiplier));
-		height: calc(40px * var(--multiplier));
-		background-color: #fff;
-		border-radius: 50%;
-		transition: 0.3s ease bottom;
-	}
-
-	#moon:before {
-		content: '';
-		position: absolute;
-		top: calc(-12px * var(--multiplier));
-		left: calc(-17px * var(--multiplier));
-		width: calc(40px * var(--multiplier));
-		height: calc(40px * var(--multiplier));
-		background-color: #03a9f4;
-		border-radius: 50%;
-		transition: 0.3s ease background-color;
-	}
-
-	#toggle_checkbox:checked + label {
-		background-color: var(--clr_grey_shade_a);
-	}
-
-	#toggle_checkbox:checked + label #star {
-		top: calc(3px * var(--multiplier));
-		left: calc(64px * var(--multiplier));
-		transform: scale(0.3);
-		background-color: yellow;
-	}
-
-	#toggle_checkbox:checked + label .star {
-		color: yellow;
-	}
-
-	#toggle_checkbox:checked + label #moon {
-		bottom: calc(8px * var(--multiplier));
-	}
-
-	#toggle_checkbox:checked + label #moon:before {
-		background-color: var(--clr_grey_shade_a);
+	.theme_toggle button:active {
+		transform: translateY(0.125rem);
 	}
 </style>
--- src/routes/(default)/+layout.svelte
+++ src/routes/(default)/+layout.svelte
@@ -1,5 +1,40 @@
 <script>
+	import { onMount } from 'svelte';
+	import { themeStore } from '../../stores';
 	import ReusableLayout from '$lib/components/ReusableLayout.svelte';
+
+	onMount(() => {
+		function set_theme(theme) {
+			localStorage.setItem('theme', theme);
+			themeStore.set(theme);
+			document.body.dataset.theme = theme;
+		}
+
+		// check localStorage for theme pref
+		const stored_theme = localStorage.getItem('theme');
+
+		// set theme on layout mount
+		if (stored_theme) {
+			if (stored_theme === 'dark') {
+				set_theme('dark');
+			} else {
+				set_theme('light');
+			}
+		} else {
+			// no theme pref in localStorage
+			// check sys theme pref
+			const prefers_dark = window.matchMedia('(prefers-color-scheme: dark)').matches;
+			if (prefers_dark) {
+				set_theme('dark');
+			} else {
+				set_theme('light');
+			}
+		}
+
+		themeStore.subscribe((value) => {
+			set_theme(value);
+		});
+	});
 </script>
 
 <ReusableLayout>
