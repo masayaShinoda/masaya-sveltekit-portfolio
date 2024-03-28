4da9f15ac1540bc58b5faa3974222af298a3bfde
masayaShinoda <masayashida@gmail.com>
2024-03-28 10:08:55 +0700

    specified adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,9 +1,11 @@
+import adapter from '@sveltejs/adapter-auto';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	preprocess: vitePreprocess(),
 	kit: {
+		adapter: adapter(),
 		prerender: {
 			handleMissingId: 'warn',
 			handleHttpError: 'warn',
