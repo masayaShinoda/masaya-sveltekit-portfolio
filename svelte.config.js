b918dd4b6ee452c8a34c91ab18caab31806d5f3e
masayaShinoda <masayashida@gmail.com>
2024-05-17 11:59:11 +0700

    add preprocess fix

--- svelte.config.js
+++ svelte.config.js
@@ -1,10 +1,16 @@
 import adapter from '@sveltejs/adapter-auto';
+import { vitePreprocess } from '@sveltejs/kit/vite';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
+	preprocess: [vitePreprocess()],
 	kit: {
-		adapter: adapter()
+		adapter: adapter(),
+		prerender: {
+			handleMissingId: 'warn',
+			handleHttpError: 'warn',
+		}
 	}
 };
 
-export default config;
+export default config; 
