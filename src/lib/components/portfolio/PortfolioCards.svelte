6b98a39c0741167418151a0221cc243b16874273
masayaShinoda <masayashida@gmail.com>
2024-06-16 14:56:34 +0700

    add portfolio cards

--- src/lib/components/portfolio/PortfolioCards.svelte
+++ src/lib/components/portfolio/PortfolioCards.svelte
@@ -2,7 +2,76 @@
 	class="mx-auto mb-32 flex max-w-content-max flex-col-reverse items-center justify-between gap-6 rounded-xl p-horizontal md:flex-row md:rounded-4xl"
 	style="background: linear-gradient(106.93deg, rgba(255, 255, 255, 0.05) 0%, rgba(0, 119, 204, 0.05) 100%);"
 >
-	<div class="mb-4 flex flex-col items-start md:mb-0 md:max-w-[360px]">
-		<h1 class="mb-4 text-scale-4 text-primary dark:text-secondary-shade-b">Portfolio</h1>
+	<div class="mb-4 flex flex-col items-start md:mb-0">
+		<h1 class="mb-6 text-scale-4 text-primary dark:text-secondary-shade-b">Portfolio</h1>
+		<div class="flex grid-cols-2 flex-col gap-8 sm:grid lg:grid-cols-3">
+			<section class="rounded-3xl bg-white p-6 dark:bg-black">
+				<img
+					src="/images/icon-code.svg"
+					width="48"
+					height="48"
+					style="filter: brightness(0) saturate(0) var(--filter-clr-secondary);"
+					alt="Web development icon"
+					class="mb-6"
+				/>
+				<h2 class="mb-3 text-scale-2 text-primary">Web development</h2>
+				<p class="mb-6 text-grey-5">
+					I see the web as an important and democratizing platform for people around the world to
+					share their ideas and to show who they are and what they care about, where anyone with an
+					internet connection can access.
+				</p>
+				<a
+					href="/portfolio"
+					class="w-full inline-block rounded-full border border-secondary bg-gradient-secondary p-4 text-center text-scale-0 text-grey-1 shadow-convex transition-all hover:brightness-105 active:translate-y-px dark:text-grey-6 sm:w-auto"
+				>
+					See my projects
+				</a>
+			</section>
+			<section class="rounded-3xl bg-white p-6 dark:bg-black">
+				<img
+					src="/images/icon-explorer.svg"
+					width="48"
+					height="48"
+					style="filter: brightness(0) saturate(0) var(--filter-clr-secondary);"
+					alt="UI/UX design icon"
+					class="mb-6"
+				/>
+				<h2 class="mb-3 text-scale-2 text-primary">UI/UX Design</h2>
+				<p class="mb-6 text-grey-5">
+					My approach to UI/UX design focuses on creating intuitive experiences that meet users'
+					needs, allowing them to achieve their goals quickly. Through research, prototyping, and
+					user testing, I strive to bridge the gap between users' goals and digital solutions.
+				</p>
+				<a
+					href="/portfolio"
+					class="w-full inline-block rounded-full border border-secondary bg-gradient-secondary p-4 text-center text-scale-0 text-grey-1 shadow-convex transition-all hover:brightness-105 active:translate-y-px dark:text-grey-6 sm:w-auto"
+				>
+					See my projects
+				</a>
+			</section>
+			<section class="rounded-3xl bg-white p-6 dark:bg-black">
+				<img
+					src="/images/icon-pen.svg"
+					width="48"
+					height="48"
+					style="filter: brightness(0) saturate(0) var(--filter-clr-secondary);"
+					alt="Graphic design icon"
+					class="mb-6"
+				/>
+				<h2 class="mb-3 text-scale-2 text-primary">Graphic Design</h2>
+				<p class="mb-6 text-grey-5">
+					I use graphic design to communicate key ideas with clarity and impact. Prioritizing
+					readability and practicality, I create designs that are both visually compelling and easy
+					to understand. My goal is to transform concepts into visuals that engage and inform
+					audiences effectively.
+				</p>
+				<a
+					href="/portfolio"
+					class="w-full inline-block rounded-full border border-secondary bg-gradient-secondary p-4 text-center text-scale-0 text-grey-1 shadow-convex transition-all hover:brightness-105 active:translate-y-px dark:text-grey-6 sm:w-auto"
+				>
+					See my projects
+				</a>
+			</section>
+		</div>
 	</div>
 </section>
